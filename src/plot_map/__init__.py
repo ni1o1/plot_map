a90d4aa684a2ba3383aad98045a66d6878a67f91
余庆 <yuqing@yuqingdeMacBook-Air-4.local>
2021-10-21 11:25:03 +0800

    0.3.0

--- src/plot_map/__init__.py
+++ src/plot_map/__init__.py
@@ -1,6 +1,2 @@
 from .plot_map import plot_map,plotscale
-from .CoordinatesConverter import gcj02tobd09,bd09togcj02,wgs84togcj02,gcj02towgs84,wgs84tobd09,bd09towgs84,getdistance
-from .grids import *
-from .ckdnearest import *
-from .odprocess import *
-__version__ = '0.2.9'
+__version__ = '0.3.0'
